03f332ac3acd4cc9776f6ce62701822f2fc41d58
Laszlo <11998934+laszlo-san@users.noreply.github.com>
2024-07-22 12:33:26 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -119,6 +119,7 @@ sudo apt -qq install -y kubectl;
 
 log_step "Install kubectl autocomplete";
 source /usr/share/bash-completion/bash_completion;
+echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc;
 echo 'complete -o default -F __start_kubectl k' >>~/.bashrc;
 # shellcheck source="$HOME/.bashrc"
